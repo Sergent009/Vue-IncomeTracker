d1a7bb98707db2d230a01dd23e36850906c70959
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2024-01-24 15:06:34 +0500

    Project is further exceeded

--- src/components/AddTransaction2.vue
+++ src/components/AddTransaction2.vue
@@ -1,15 +1,15 @@
 <template>
  
     <h3>Add new transaction</h3>
-    <form id="form">
+    <form id="form" @submit.prevent="onSubmit">
         <div class="form-control">
             <label for="text">Text</label>
-            <input type="text" id="text" placeholder="Enter text...">
+            <input type="text" v-model="text" id="text" placeholder="Enter text...">
         </div>
 
         <div class="form-control">
             <label for="amount">Amount<br />(negative - expense, positive - income)</label>
-            <input type="number" id="amount" placeholder="Enter Amount...">
+            <input type="text" id="amount" v-model="amount" placeholder="Enter Amount...">
         </div>
 
         <button class="btn">Add transaction</button>
@@ -18,9 +18,34 @@
 </template>
 
 <script>
+import {ref} from 'vue' 
+// import useToast from 'vue-toastification'
 
     export default {
         name: 'AddTransaction2',
+
+        setup(){
+            const text = ref('')
+            const amount = ref('')
+
+            // const toast = useToast()
+
+
+            const onSubmit = () => {
+                if(!text.value || !amount.value){
+                    // toast.error('Both fields should be filled properly.')
+                    throw new Error("Both fields should be filled properly")
+                }
+                text.value = ''
+                amount.value = ''
+            }
+
+            return{
+                onSubmit,
+                text,
+                amount
+            }
+        }
     }
 </script>
 
